13926509fe2c8b77c21a118d039957d0d2befe39
DendyRamdhani <dendiepie3@gmail.com>
2021-09-29 22:27:40 +0700

    first commit

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    PlayerShooting playerShooting;
+    bool isDead;                                                
+    bool damaged;                                               
+
+    
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooting = GetComponentInChildren<PlayerShooting>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //jika player terkena damage
+        if (damaged)
+        {
+            //jika benar rubah warna menjadi flash color
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            //menghilangkan damageimage
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        //set damage false
+        damaged = false;
+    }
+
+    //fungsi unutk mendapatkan damage
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        //mengurangi damage
+        currentHealth -= amount;
+
+        //update tampilan health slider
+        healthSlider.value = currentHealth;
+
+        //memainkan efek suara ketika terkena damage
+        playerAudio.Play();
+
+        //meanggil method Death() jika darah kurang dari atau sama dengan 10
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        playerShooting.DisableEffects();
+
+        anim.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }
+    public void RestartLevel()
+    {
+        //meload ulang scene dengan index 0 pada build setting
+        SceneManager.LoadScene(0);
+    }
+
+}
